d11180662fd5133034eb584d8dc38f6298e592cf
Arístides Pérez <aristp.2005@gmail.com>
2025-10-12 18:43:22 -0400

    feat(app): add secure storage service provider

--- apps/app/lib/src/utils/services/prefs_service.dart
+++ apps/app/lib/src/utils/services/prefs_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -6,4 +7,10 @@ part 'prefs_service.g.dart';
 @Riverpod(keepAlive: true)
 SharedPreferencesAsync prefs(Ref ref) {
   return SharedPreferencesAsync();
+}
+
+FlutterSecureStorage securePrefs(Ref ref) {
+  return const FlutterSecureStorage(
+    aOptions: AndroidOptions(encryptedSharedPreferences: true),
+  );
 }
