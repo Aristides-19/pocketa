b639b931533aadca819c3f02a8b30b42b55624a5
Arístides Pérez <aristp.2005@gmail.com>
2025-10-18 00:21:00 -0400

    style(app): adjust primary button shadow

--- apps/app/lib/src/widgets/button/primary_button.dart
+++ apps/app/lib/src/widgets/button/primary_button.dart
@@ -26,9 +26,9 @@ class Button extends StatelessWidget {
         borderRadius: AppTheme.borderRadius,
         boxShadow: [
           BoxShadow(
-            color: theme.colorScheme.primary.withAlpha(100),
-            blurRadius: 100,
-            spreadRadius: 5,
+            color: theme.colorScheme.primary.withAlpha(50),
+            blurRadius: 10,
+            spreadRadius: 1,
             offset: const Offset(0, 10),
           ),
         ],
