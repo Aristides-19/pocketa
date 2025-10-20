4437ca4781486fdbc34bf8c1b9adcbb271ca3ee9
Arístides Pérez <aristp.2005@gmail.com>
2025-10-20 02:00:32 -0400

    feat(app): refactor ErrorScreen to handle loading state onRetry

--- apps/app/lib/src/widgets/layout/error_screen.dart
+++ apps/app/lib/src/widgets/layout/error_screen.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:pocketa/src/localization/locale.dart';
 import 'package:pocketa/src/widgets/widgets.dart';
 
-class ErrorScreen extends StatelessWidget {
+class ErrorScreen extends HookWidget {
   const ErrorScreen({
     super.key,
-    required this.onPressed,
-    required this.isLoading,
+    required this.onRetry,
     this.title,
     this.message,
   });
 
   final String? title;
   final String? message;
-  final VoidCallback onPressed;
-  final bool isLoading;
+  final Future<void> Function() onRetry;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final systemPadding = MediaQuery.of(context).padding;
+    final isLoading = useState(false);
 
     return Center(
       child: Padding(
@@ -50,8 +50,15 @@ class ErrorScreen extends StatelessWidget {
             const SizedBox(height: 28),
             POutlinedButton(
               label: LocaleKeys.actions_retry.tr(),
-              onPressed: onPressed,
-              isLoading: isLoading,
+              onPressed: () async {
+                isLoading.value = true;
+                try {
+                  await onRetry();
+                } finally {
+                  isLoading.value = false;
+                }
+              },
+              isLoading: isLoading.value,
               fontSize: 18,
               height: 54,
             ),
