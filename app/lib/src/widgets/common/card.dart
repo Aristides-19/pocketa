8abd87e207fdc2af9cdeee3c2c89135a247ccf6d
Arístides Pérez <aristp.2005@gmail.com>
2025-12-25 20:52:32 -0400

    feat(app): enhance CommonCard widget with selected state

--- app/lib/src/widgets/common/card.dart
+++ app/lib/src/widgets/common/card.dart
@@ -2,27 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:pocketa/src/constants/constants.dart';
 
 class CommonCard extends StatelessWidget {
-  const CommonCard({super.key, required this.child, this.padding});
+  const CommonCard({
+    super.key,
+    required this.child,
+    this.padding = const .symmetric(horizontal: 18, vertical: 12),
+    this.selected = false,
+  });
 
   final Widget child;
-  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry padding;
+  final bool selected;
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
+    final colorScheme = Theme.of(context).colorScheme;
 
     return Card(
       margin: .zero,
       elevation: 0,
-      color: theme.colorScheme.surfaceContainer,
+      color: colorScheme.surfaceContainer,
       shape: RoundedRectangleBorder(
         borderRadius: AppTheme.borderRadius,
-        side: BorderSide(color: theme.colorScheme.outline),
+        side: BorderSide(
+          color: selected
+              ? colorScheme.primary.withAlpha(120)
+              : colorScheme.outline,
+        ),
       ),
-      child: Padding(
-        padding: padding ?? const .symmetric(horizontal: 18, vertical: 12),
-        child: child,
-      ),
+      child: Padding(padding: padding, child: child),
     );
   }
 }
